fe8fa7a7a3865fc38fd8e1995563143a24970360
deividas strole <deiwis@yahoo.com>
2025-01-10 16:12:14 -0800

    constructor functions

--- starter/script.js
+++ starter/script.js
@@ -1,2 +1,21 @@
 'use strict';
-//oop
+
+// Construction functions
+const Person = function (firstName, birthYear) {
+  this.firstName = firstName;
+  this.birthYear = birthYear;
+};
+
+const d = new Person('Deividas', 2000);
+console.log(d);
+
+// 1. New empty {} object created
+// 2. function is called, this = {}
+// 3. {} linked to prototype
+// 4. function automatically return {}
+
+const mikas = new Person('Mikas', 3000);
+
+console.log(mikas);
+
+console.log(mikas instanceof Person);
