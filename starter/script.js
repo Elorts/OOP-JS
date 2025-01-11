7988d1e2c7d1a4438d502bf1e31050cd92048491
deividas strole <deiwis@yahoo.com>
2025-01-11 09:18:59 -0800

    prototype

--- starter/script.js
+++ starter/script.js
@@ -6,8 +6,8 @@ const Person = function (firstName, birthYear) {
   this.birthYear = birthYear;
 };
 
-const d = new Person('Deividas', 2000);
-console.log(d);
+const deividas = new Person('Deividas', 2000);
+console.log(deividas);
 
 // 1. New empty {} object created
 // 2. function is called, this = {}
@@ -19,3 +19,19 @@ const mikas = new Person('Mikas', 3000);
 console.log(mikas);
 
 console.log(mikas instanceof Person);
+
+console.log('************* Prototypes <220>***********');
+
+//console.log(Person.prototype);
+
+Person.prototype.calAge = function () {
+  console.log(1900 + this.birthYear);
+};
+
+deividas.calAge();
+
+mikas.calAge();
+
+console.log(deividas.__proto__);
+
+console.log(Person.prototype.isPrototypeOf(deividas));
