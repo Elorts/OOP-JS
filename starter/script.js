85f58c553681d6ccd9b0994712526dec87412780
deividas strole <deiwis@yahoo.com>
2025-01-11 16:05:43 -0800

    Object.create

--- starter/script.js
+++ starter/script.js
@@ -201,6 +201,11 @@ const PersonProto = {
   calcAge() {
     console.log(2037 - this.birthYear);
   },
+
+  init(firstName, birthYear) {
+    this.firstName = firstName;
+    this.birthYear = birthYear;
+  },
 };
 
 const steven = Object.create(PersonProto);
@@ -208,3 +213,7 @@ const steven = Object.create(PersonProto);
 steven.name = 'Steven';
 steven.birthYear = 1889;
 steven.calcAge();
+
+const felix = Object.create(PersonProto);
+felix.init('Felix', 1925);
+felix.calcAge();
