446fd7cbc19b8c788f2a5a913038f522c092e8ae
deividas strole <deiwis@yahoo.com>
2025-01-12 10:18:47 -0800

    Inheritance Between "Classes": Object.create

--- starter/script.js
+++ starter/script.js
@@ -372,7 +372,7 @@ tesla.brake();
 tesla.chargeBattery(100);
 console.log(tesla);
 */
-
+/*
 class PersonCl {
   constructor(fullName, birthYear) {
     this.fullName = fullName;
@@ -425,3 +425,34 @@ class StudentCl extends PersonCl {
 const maryte = new StudentCl('Maryte Melninkaite', 2020, 'KPSS History');
 maryte.introduce();
 maryte.calcAge();
+*/
+console.log('~~~~ Inheritance Between "Classes": Object.create ~~~~');
+
+const PersonProto = {
+  calcAge() {
+    console.log(2037 - this.birthYear);
+  },
+
+  init(firstName, birthYear) {
+    this.firstName = firstName;
+    this.birthYear = birthYear;
+  },
+};
+
+const steven = Object.create(PersonProto);
+
+const StudentProto = Object.create(PersonProto);
+
+StudentProto.init = function (firstName, birthYear, course) {
+  PersonProto.init.call(this, firstName, birthYear);
+  this.course = course;
+};
+
+StudentProto.introduce = function () {
+  console.log(`My name is ${this.firstName} and I'm studying ${this.course}`);
+};
+
+const jay = Object.create(StudentProto);
+jay.init('Jay', 2010, 'Computer Science');
+jay.introduce();
+jay.calcAge();
